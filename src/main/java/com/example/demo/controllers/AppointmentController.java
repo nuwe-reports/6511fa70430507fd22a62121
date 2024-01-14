5d14cbaa57b0ea5fbf26dc378d3f830a16eac09b
Mariquena Allosa <allosamariquena@gmail.com>
2024-01-13 23:14:59 -0300

    is still necessary resolve a test

--- src/main/java/com/example/demo/controllers/AppointmentController.java
+++ src/main/java/com/example/demo/controllers/AppointmentController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ public class AppointmentController {
 
     @Autowired
     AppointmentRepository appointmentRepository;
+
 
     @GetMapping("/appointments")
     public ResponseEntity<List<Appointment>> getAllAppointments(){
@@ -53,20 +55,33 @@ public class AppointmentController {
     @PostMapping("/appointment")
     public ResponseEntity<List<Appointment>> createAppointment(@RequestBody Appointment appointment){
 
+        try {
+            if(appointment == null){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
 
-        // Check if the appointment overlaps with existing appointments
-        if (isOverlappingWithExisting(appointment)) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            if (isSameAppointment(appointment)){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+
+            // Check if the appointment overlaps with existing appointments
+            if (isOverlappingWithExisting(appointment)) {
+                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+            }
+
+            // Save the appointment to the database
+            appointmentRepository.save(appointment);
+
+            // Get and return the updated list of appointments
+            List<Appointment> appointments = new ArrayList<>();
+            appointmentRepository.findAll().forEach(appointments::add);
+
+            return new ResponseEntity<>(appointments, HttpStatus.OK);
+        } catch (Exception e) {
+            // Handle other exceptions, if any
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
 
-        // Save the appointment to the database
-        appointmentRepository.save(appointment);
-
-        // Get and return the updated list of appointments
-        List<Appointment> appointments = new ArrayList<>();
-        appointmentRepository.findAll().forEach(appointments::add);
-
-        return new ResponseEntity<>(appointments, HttpStatus.OK);
     }
 
     private boolean isOverlappingWithExisting(Appointment newAppointment) {
@@ -82,6 +97,27 @@ public class AppointmentController {
 
         return false;
     }
+
+    private boolean isSameAppointment(Appointment appointment) {
+
+        // Retrieve all existing appointments
+        List<Appointment> existingAppointments = appointmentRepository.findAll();
+
+        // Check for overlap with each existing appointment
+        for (Appointment existingAppointment : existingAppointments) {
+            if ((appointment.getPatient().equals(existingAppointment.getPatient()))
+                    && (appointment.getDoctor().equals(existingAppointment.getDoctor()))
+                    && (appointment.getRoom().equals(existingAppointment.getRoom())
+                    && (appointment.getStartsAt().isEqual(existingAppointment.getStartsAt()))
+                    && (appointment.getFinishesAt().isEqual(existingAppointment.getFinishesAt()))) ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
     @DeleteMapping("/appointments/{id}")
     public ResponseEntity<HttpStatus> deleteAppointment(@PathVariable("id") long id){
 
